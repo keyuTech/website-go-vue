1aa6a985532625272a08fbead549b209a9253f3f
keyu <me@keyu.email>
2023-07-01 18:51:21 +0800

    feat: init & add config

---
+++ web/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl">111111</h1>
+    <HelloWorld msg="2222" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+</style>
